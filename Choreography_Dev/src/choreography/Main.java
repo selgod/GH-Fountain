ebdb732faeda6d7cb0e7eb56074ed3598cce7a28
offringm <offringm@mail.gvsu.edu>
2014-09-25 00:57:33 -0400

    Add scrollbar to primary stage
    Makes main window scrollable vertically, needs to be polishes and have
    horizontal scrollbar added still

--- Choreography_Dev/src/choreography/Main.java
+++ Choreography_Dev/src/choreography/Main.java
@@ -3,13 +3,20 @@
 package choreography;
 	
 import choreography.model.fountain.Fountain;
+
 import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import choreography.model.*;
+import javafx.scene.control.ScrollBar;
 import javafx.scene.image.Image;
 
 /**
@@ -51,16 +58,33 @@ public class Main extends Application {
             primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/resources/ghmf_cs_logo.png")));
 //            primaryStage.setIconified(true);
             this.setPrimaryStage(primaryStage);
-            
+            Group grp = new Group();
+            ScrollBar sc = new ScrollBar();
             Main.primaryStage = primaryStage;
             primaryStage.setTitle("GHMF Choreography Studio");
             fountain = Fountain.getInstance();
             FXMLLoader fxml = new FXMLLoader(getClass().getResource("view/Choreography.fxml"));
             root = (VBox)fxml.load();
-            Scene scene = new Scene(root);
+            grp.getChildren().addAll(root, sc);
+            Scene scene = new Scene(grp, 1330, 780);
+            
+            
             scene.getStylesheets().add(getClass().getResource("view/application.css").toExternalForm());
             primaryStage.setScene(scene);
-            primaryStage.setResizable(false);
+            primaryStage.setResizable(true);           
+            sc.setLayoutX(scene.getWidth()-sc.getWidth());
+            sc.setMin(0);
+            sc.setOrientation(Orientation.VERTICAL);
+            sc.setPrefHeight(scene.getHeight()-2);
+            sc.setMax(360);
+            
+            sc.valueProperty().addListener(new ChangeListener<Number>() {
+                public void changed(ObservableValue<? extends Number> ov,
+                    Number old_val, Number new_val) {
+                        root.setLayoutY(-new_val.doubleValue());
+                }
+            });
+            
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
